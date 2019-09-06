c2bed4fc113f03a113ed75370f08ab5c76fd7ebc
Piotr Chowaniec <piotrchowaniec@poczta.fm>
2019-09-06 21:51:17 +0200

    v 0.2

--- js/game.js
+++ js/game.js
@@ -1,1 +1,27 @@
-// Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
+const gameSummary = {
+    numbers: 0,
+    win: 0,
+    losses: 0,
+    draws: 0,
+};
+
+const game = {
+    playerHand: "",
+    aiHand: "",
+
+};
+
+const hands = [...document.querySelectorAll(".select img")];
+
+function handSelection() {
+    console.log(this);
+    game.playerHand = this.dataset.option;
+    hands.forEach(hands => hands.style.boxShadow = '')
+    console.log(game.playerHand);
+    this.style.boxShadow = '0 0 0 4px red'
+}
+// handSelection = (e) => {
+//     console.log(e.target)
+// }
+
+hands.forEach(hand => hand.addEventListener("click", handSelection));
